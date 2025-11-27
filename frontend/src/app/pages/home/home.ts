d00fbf809b80e9912ea71f612179783efdfbd907
sami51100 <samidriouche@yahoo.fr>
2025-11-27 11:16:16 +0100

    Adding a connection with the backend to create and get the rooms.

--- frontend/src/app/pages/home/home.ts
+++ frontend/src/app/pages/home/home.ts
@@ -1,20 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { RoomCard } from '../../components/room-card/room-card';
 import { Navbar } from '../../components/navbar/navbar';
 import {MatIconModule} from '@angular/material/icon';
-
+import { RoomService } from '../../service/room/room.service';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-home',
   standalone: true,
   templateUrl: './home.html',
   styleUrls: ['./home.css'],
-  imports: [RoomCard , MatIconModule , Navbar],
+  imports: [RoomCard , MatIconModule , Navbar , CommonModule],
 })
 
-export class HomeComponent {
 
-  constructor(private router: Router) {}
+export class HomeComponent implements OnInit {
+
+  rooms: any[] = [];  // <-- liste des rooms
+
+  constructor(
+    private router: Router,
+    private roomService: RoomService
+  ) {}
+
+  ngOnInit() {
+  this.roomService.rooms$.subscribe(data => this.rooms = data);
+  this.roomService.getRooms(); // charge initialement
+  }
 
   goToRooms() {
     this.router.navigate(['/room']);
