7110fd7735387e0ae57a9a533d2f30efb2d20284
CrazyLeek <ok@exemple>
2025-11-27 14:36:16 +0100

    Add host name info for public rooms

--- backend/skullking/src/main/java/com/example/skullking/entities/RoomDTOForGuests.java
+++ backend/skullking/src/main/java/com/example/skullking/entities/RoomDTOForGuests.java
@@ -9,6 +9,7 @@ public class RoomDTOForGuests {
     public UUID uuid;
     public String name;
     public int nbPlayers;
+    public String hostName;
 
 
 
@@ -17,6 +18,7 @@ public class RoomDTOForGuests {
         this.uuid = room.getUuid();
         this.name = room.getName();
         this.nbPlayers = room.countPlayers();
+        this.hostName = room.getHost().getName();
     }
 
 }
