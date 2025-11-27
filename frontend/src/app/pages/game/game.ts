6b9c603e272c027201d3b8cfebc9e13a948f4444
AlexandreDahi <ad.alexandredahi@gmail.com>
2025-11-27 10:41:42 +0100

    dev: adding Spreads for cards and letting you create as many cards as you want

--- frontend/src/app/pages/game/game.ts
+++ frontend/src/app/pages/game/game.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { Cards } from '../../components/cards/cards';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-game',
-  imports: [Cards],
+  imports: [Cards,CommonModule],
   templateUrl: './game.html',
   styleUrl: './game.css',
 })
 export class Game {
+  cardIds: number[] = [1, 0, 2, 3];
 
 }
