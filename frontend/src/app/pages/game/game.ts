a5e6869e164926475a3c40853a32e7dfe197d2cd
AlexandreDahi <ad.alexandredahi@gmail.com>
2025-11-28 14:18:23 +0100

    fix: adding otherPlayers in game.ts

--- frontend/src/app/pages/game/game.ts
+++ frontend/src/app/pages/game/game.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { PlaceBet } from '../../components/place-bet/place-bet';
 import { ExpandablePlaceBet } from '../../components/expandable-place-bet/expandable-place-bet';
 import { Hand } from '../../components/hand/hand';
+import {OtherPlayers} from '../../components/other-players/other-players';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-game',
-  imports: [Hand, CommonModule, PlaceBet, ExpandablePlaceBet, ],
+  imports: [Hand, CommonModule, PlaceBet, ExpandablePlaceBet, OtherPlayers ],
   templateUrl: './game.html',
   styleUrl: './game.css',
 })
