f2fe794acba1a28610d58870f9fe8607faa6928b
AlexandreDahi <ad.alexandredahi@gmail.com>
2025-12-01 14:32:08 +0100

    dev: continuation de la main

--- frontend/src/app/pages/game/game.ts
+++ frontend/src/app/pages/game/game.ts
@@ -3,10 +3,11 @@ import { PlaceBet } from '../../components/place-bet/place-bet';
 import { ExpandablePlaceBet } from '../../components/expandable-place-bet/expandable-place-bet';
 import { Hand } from '../../components/hand/hand';
 import { CommonModule } from '@angular/common';
+import { OtherPlayers } from '../../components/other-players/other-players';
 
 @Component({
   selector: 'app-game',
-  imports: [Hand, CommonModule, PlaceBet, ExpandablePlaceBet, ],
+  imports: [Hand, CommonModule, PlaceBet, ExpandablePlaceBet, OtherPlayers ],
   templateUrl: './game.html',
   styleUrl: './game.css',
 })
