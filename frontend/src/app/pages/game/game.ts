190cd57d8663684cda5021737ac512b0c50fce87
sami51100 <samidriouche@yahoo.fr>
2025-12-02 15:44:03 +0100

    Test mode drop zone

--- frontend/src/app/pages/game/game.ts
+++ frontend/src/app/pages/game/game.ts
@@ -3,13 +3,14 @@ import { PlaceBet } from '../../components/place-bet/place-bet';
 import { ExpandablePlaceBet } from '../../components/expandable-place-bet/expandable-place-bet';
 import { Hand } from '../../components/hand/hand';
 import { DropZone } from '../../components/drop-zone/drop-zone';
+import { TestDrop } from '../../components/test-drop/test-drop';  
 import {OtherPlayers} from '../../components/other-players/other-players';
 import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-game',
-  imports: [Hand, CommonModule, PlaceBet, ExpandablePlaceBet, OtherPlayers , DropZone],
+  imports: [Hand, CommonModule, PlaceBet, ExpandablePlaceBet, OtherPlayers , DropZone, TestDrop],
   templateUrl: './game.html',
   styleUrl: './game.css',
 })
--- frontend/src/app/service/websocket/websocket.service.ts
+++ frontend/src/app/service/websocket/websocket.service.ts
@@ -1,68 +1,68 @@
-import { Injectable } from '@angular/core';
+// import { Injectable } from '@angular/core';
 
-import { IMessage, RxStomp, RxStompConfig } from "@stomp/rx-stomp";
-import { Observable } from 'rxjs';
-
-
-@Injectable({
-  providedIn: 'root',
-})
-export class WebSocketService {
+// import { IMessage, RxStomp, RxStompConfig } from "@stomp/rx-stomp";
+// import { Observable } from 'rxjs';
 
 
-    private config: RxStompConfig = {
-        brokerURL: "ws://localhost:8080/games",
-        reconnectDelay: 2000,
-        heartbeatIncoming: 10000,
-        heartbeatOutgoing: 10000,
-    }
-
-    private rxStomp = new RxStomp()
-
-    private roomUuid = ""
-    private playerUuid = ""
-    private playerToken = ""
+// @Injectable({
+//   providedIn: 'root',
+// })
+// export class WebSocketService {
 
 
-    private publicChannel = new Observable<IMessage>()
-    private privateChannel = new Observable<IMessage>()
+//     private config: RxStompConfig = {
+//         brokerURL: "ws://localhost:8080/games",
+//         reconnectDelay: 2000,
+//         heartbeatIncoming: 10000,
+//         heartbeatOutgoing: 10000,
+//     }
+
+//     private rxStomp = new RxStomp()
+
+//     private roomUuid = ""
+//     private playerUuid = ""
+//     private playerToken = ""
 
 
-    private activateWebSocket() {
-        this.rxStomp.configure(this.config)
-        this.rxStomp.activate()
-
-    } 
-
-    joinRoom(roomUuid: string, playerUuid: string, playerToken: string) {
-
-        if (!this.rxStomp.active) {
-            this.activateWebSocket()
-        }
-
-        this.roomUuid = roomUuid
-        this.playerUuid = playerUuid
-        this.playerToken = playerToken
-
-        this.publicChannel = this.rxStomp.watch({
-            destination: `/rooms/${this.roomUuid}/`
-        })
+//     private publicChannel = new Observable<IMessage>()
+//     private privateChannel = new Observable<IMessage>()
 
 
-        this.privateChannel = this.rxStomp.watch({
-            destination: `/rooms/${this.roomUuid}/users/${this.playerUuid}/${this.playerToken}`
-        })
+//     private activateWebSocket() {
+//         this.rxStomp.configure(this.config)
+//         this.rxStomp.activate()
 
-    }
+//     } 
+
+//     joinRoom(roomUuid: string, playerUuid: string, playerToken: string) {
+
+//         if (!this.rxStomp.active) {
+//             this.activateWebSocket()
+//         }
+
+//         this.roomUuid = roomUuid
+//         this.playerUuid = playerUuid
+//         this.playerToken = playerToken
+
+//         this.publicChannel = this.rxStomp.watch({
+//             destination: `/rooms/${this.roomUuid}/`
+//         })
 
 
-    getPublicChannel() {
-        return this.publicChannel
-    }
+//         this.privateChannel = this.rxStomp.watch({
+//             destination: `/rooms/${this.roomUuid}/users/${this.playerUuid}/${this.playerToken}`
+//         })
 
-    getPrivateChannel() {
-        return this.privateChannel
-    }
+//     }
+
+
+//     getPublicChannel() {
+//         return this.publicChannel
+//     }
+
+//     getPrivateChannel() {
+//         return this.privateChannel
+//     }
 
 
 
@@ -70,4 +70,4 @@ export class WebSocketService {
    
 
 
-}
+// }
